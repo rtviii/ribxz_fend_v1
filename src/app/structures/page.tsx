d7a6db0640a18e260009bf45cb88a85bab0cdaf9
rtviii <rtkushner@gmail.com>
2024-05-20 19:20:41 -0700

    sane scrolling + overflow

--- src/app/structures/page.tsx
+++ src/app/structures/page.tsx
@@ -10,42 +10,49 @@ import { RibosomeStructure, useRoutersRouterStructListStructuresQuery } from "@/
 import { useEffect } from "react"
 import FilterSidebar from "./filters"
 import { SheetDemo } from "@/components/sidebar"
-
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 
 
 
 export default function StructureCatalogue() {
-
-  const { data, isLoading }:{data:RibosomeStructure[], isLoading: boolean} = useRoutersRouterStructListStructuresQuery()
-  useEffect(() => {
-    console.log(data);
-  }, [data])
+  const { data, isLoading }: { data: RibosomeStructure[], isLoading: boolean } = useRoutersRouterStructListStructuresQuery()
   return (
-    <div className="max-w-screen max-h-screen block overflow-hidden">
-      <h1 className="text-2xl font-bold mb-6 p-4" >Ribosome Structures</h1>
-      <div className="grid grid-cols-12 gap-4 mb-4">
-        <div className="col-span-3 border-gray-200 pr-4">
-          <FilterSidebar />
-          <SheetDemo/>
+    <div className="max-w-screen max-h-screen min-h-screen p-4 flex flex-col flex-grow  outline ">
+      <h1 className="text-2xl font-bold mb-6 " >Ribosome Structures</h1>
 
-          <div> PAGINATION</div>
-          <div className="flex justify-center space-x-2">
-            <Button className="bg-[#eaeaea] text-xs">1</Button>
-            <Button className="bg-[#eaeaea] text-xs">2</Button>
-            <Button className="bg-[#eaeaea] text-xs">3</Button>
-            <Button className="bg-[#eaeaea] text-xs">4</Button>
-            <Button className="bg-[#eaeaea] text-xs">5</Button>
-            <span>...</span>
-            <Button className="bg-[#eaeaea] text-xs">56</Button>
-            <Button className="bg-[#eaeaea] text-xs">{`>`}</Button>
-          </div>
-        </div>
+      <div className="grow"  >
 
-        <div className="col-span-9 pl-4">
-          <div className="flex flex-row gap-2 mb-6 min-w-full flex-wrap">
-            {isLoading === false ? data.slice(0,18).map(struct => <StructureCard _={struct} key={struct.rcsb_id} />) : null}
+        <div className="grid grid-cols-12 gap-4 min-h-[90vh]    ">
+
+          <div className="col-span-3  flex flex-col min-h-full pr-4">
+            <FilterSidebar />
+            <SheetDemo />
+
+            <div className="grow"> PAGINATION</div>
+
+            <div className="flex justify-center space-x-2">
+              <Button className="bg-[#eaeaea] text-xs">1</Button>
+              <Button className="bg-[#eaeaea] text-xs">2</Button>
+              <Button className="bg-[#eaeaea] text-xs">3</Button>
+              <Button className="bg-[#eaeaea] text-xs">4</Button>
+              <Button className="bg-[#eaeaea] text-xs">5</Button>
+              <span>...</span>
+              <Button className="bg-[#eaeaea] text-xs">56</Button>
+              <Button className="bg-[#eaeaea] text-xs">{`>`}</Button>
+            </div>
           </div>
+
+          <div className="col-span-9 scrollbar-hidden">
+            <ScrollArea className=" max-h-[90vh] overflow-y-scroll scrollbar-hidden" scrollHideDelay={1} >
+            <div className=" gap-4 flex  flex-wrap  p-1 scrollbar-hidden"  >
+              {isLoading === false ? data.slice(0, 18).map(struct => <StructureCard _={struct} key={struct.rcsb_id} />) : null}
+            </div>
+          </ScrollArea>
+          </div>
+
+
+
         </div>
       </div>
     </div>
