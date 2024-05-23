4c0eb9036d4fdbce534327b45c8367862e8801c5
rtviii <rtkushner@gmail.com>
2024-05-22 19:08:16 -0700

    debounce

--- src/app/structures/page.tsx
+++ src/app/structures/page.tsx
@@ -4,7 +4,7 @@ import { SelectValue, SelectTrigger, SelectItem, SelectContent, Select } from "@
 import { Button } from "@/components/ui/button"
 import { CardContent, Card } from "@/components/ui/card"
 import StructureCard from "./structure-card"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { StructuresPagination, FilterSidebar } from "./filters"
 import { SidebarMenu } from "@/components/sidebar"
 import { ScrollArea } from "@/components/ui/scroll-area"
@@ -13,8 +13,23 @@ import { RootState, useAppDispatch } from '@/store/store';
 import { useAppSelector } from "@/store/store"
 import { useRoutersRouterStructFilterListQuery } from "@/store/ribxz_api/ribxz_api"
 
- 
 
+
+function useDebounce(value: any, delay: number): string {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
 
 
 
@@ -23,25 +38,21 @@ import { useRoutersRouterStructFilterListQuery } from "@/store/ribxz_api/ribxz_a
 export default function StructureCatalogue() {
 
   // const { data, isLoading, error } = useAutoRefetch();
-  const dispatch = useAppDispatch()
-  const { data, isLoading  } = useRoutersRouterStructFilterListQuery({ search:'complex'})
+  const dispatch          = useAppDispatch()
+  const filters           = useAppSelector((state) => state.ui.filters)
+  const debounced_filters = useDebounce(filters, 1000)
 
   useEffect(() => {
-    
-    console.log("Dispthced");
-    console.log(data);
-    
-    
-  },[data])
-  // useEffect(() => {
-  //   console.log(data);
-  // }, [data, isLoading, error])
 
-  
+  }, [debounced_filters])
+
+
   return (
     <div className="max-w-screen max-h-screen min-h-screen p-4 flex flex-col flex-grow  outline ">
       <h1 className="text-2xl font-bold mb-6 " >Ribosome Structures</h1>
+      <button type="button" onClick={()=>{}}>get structs</button>
       <div className="grow"  >
+
         <div className="grid grid-cols-12 gap-4 min-h-[90vh]    ">
           <div className="col-span-3  flex flex-col min-h-full pr-4">
             <FilterSidebar />
