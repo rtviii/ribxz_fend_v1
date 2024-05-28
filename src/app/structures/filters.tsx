3f41f21d21620c583e0011be930e41fd6f658420
rtviii <rtkushner@gmail.com>
2024-05-27 22:01:15 -0700

    we need new filters for proetins/rna. dont overengineer it

--- src/app/structures/filters.tsx
+++ src/app/structures/filters.tsx
@@ -45,24 +45,6 @@ const Group = (props: GroupProps<PolymerClassOption, false>) => (
   </div>
 );
 
-function CircleIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-    </svg>
-  )
-}
 
 
 export enum FilterType {
