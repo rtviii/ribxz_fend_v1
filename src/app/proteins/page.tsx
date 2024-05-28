8dc8f7c86eac9cb0e8420b7f18134bc908f6c2df
rtviii <rtkushner@gmail.com>
2024-05-27 22:50:20 -0700

    tabs for the components page

--- src/app/proteins/page.tsx
+++ src/app/proteins/page.tsx
@@ -16,6 +16,8 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 import { Label } from "@/components/ui/label"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
+import { PolymerTableRow } from "../[rcsb_id]/components_table"
+import { useState } from "react"
 
 function PolymerInput() {
 
@@ -23,10 +25,7 @@ function PolymerInput() {
     return <div className="grid w-full items-center gap-1.5  mb-2 border p-2 rounded-md">
 
         <div>
-
-
         </div>
-
         <Label htmlFor="input" className="font-bold text-lg mb-2"> Polymer Class</Label>
         <DropdownMenu>
             <DropdownMenuTrigger asChild className="flex items-center">
@@ -46,7 +45,12 @@ function PolymerInput() {
 }
 
 export default function PolymersPage() {
+    const [tabstate, setTabState] = useState("structure")
+
+
+
     return (
+        // This needs two tabs "by structure" and "by polymer Class"
         <div className="max-w-screen max-h-screen min-h-screen p-4 flex flex-col flex-grow  outline ">
             <HoverMenu />
             <h1 className="text-2xl font-bold mb-6 ">Polymers</h1>
@@ -64,8 +68,22 @@ export default function PolymersPage() {
                     <div className="col-span-9 scrollbar-hidden">
                         <ScrollArea className=" max-h-[90vh] overflow-y-scroll scrollbar-hidden" scrollHideDelay={1} >
                             <div className=" gap-4 flex  flex-wrap  p-1 scrollbar-hidden"  >
+                            <Tabs defaultValue="info"  >
+                                <TabsList className="grid w-full grid-cols-2">
 
+                                    {/* TODO: Add tooltip what each means */}
+                                    <TabsTrigger className="w-full" value="components">By Polymer Class</TabsTrigger>
+                                    <TabsTrigger className="w-full" value="info">By Structure</TabsTrigger>
+                                </TabsList>
+                                <TabsContent value="by_polymer_class">
+                                    components by  polymer class
+                                </TabsContent>
+                                <TabsContent value="by_structure">
+                                    components by structrre
+                                </TabsContent>
+                            </Tabs>
 
+                                {/* <PolymerTableRow/> */}
                             </div>
                         </ScrollArea>
                     </div>
