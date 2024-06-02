3c8274e30afce657560b4e49c86f3dcf691e443e
rtviii <rtkushner@gmail.com>
2024-06-02 10:12:26 -0700

    ligands table

--- src/app/ligands/page.tsx
+++ src/app/ligands/page.tsx
@@ -6,50 +6,66 @@ import { useAppSelector } from "@/store/store"
 import { ScrollArea } from "@radix-ui/react-scroll-area"
 import { useEffect } from "react"
 
-
 interface LigandRowProps {
-    ligandinfo        : {
-        chemical_id     : string,
-        chemical_name   : string,
-        formula_weight  : number,
+    ligandinfo: {
+        chemicalId: string,
+        chemicalName: string,
+        formula_weight: number,
         pdbx_description: string,
+        drugbank_id?: string,
     },
     associated_structs: string[],
 }
 
-// TODO: Ligands page (just a table) + small mention on each structure info page
-//  const LigandTableRow = (props:LigandRowProps) => {
-//     const ctx = useAppSelector(state => state.molstar.ui_plugin)
-//     const polymer = props.polymer
 
-//     return <TableRow
-//         className="hover:bg-slate-100   hover:cursor-pointer"
-//         onClick={props.connect_to_molstar_ctx ? () => { ctx == undefined ? console.log("Plugin is still loading") : selectChain(ctx!, polymer.auth_asym_id) } : undefined}
-//         onMouseEnter={props.connect_to_molstar_ctx ? () => { ctx == undefined ? console.log("Plugin is still loading") : highlightChain(ctx, polymer.asym_ids[0]) } : undefined}
-//         onMouseLeave={props.connect_to_molstar_ctx ? () => { ctx == undefined ? console.log("Plugin is still loading") : removeHighlight(ctx!) } : undefined} >
-//         <TableCell>{polymer.parent_rcsb_id}</TableCell>
-//         <TableCell>{polymer.auth_asym_id}</TableCell>
-//         <TableCell><Badge variant   = "outline">{polymer.nomenclature}</Badge></TableCell>
-//         <TableCell        className = "whitespace-pre">{polymer.src_organism_names.join(',')}</TableCell>
-//         {/* <TableCell><DeleteIcon className="h-5 w-5" /></TableCell> */}
-//     </TableRow>
-// }
+// TODO: Ligands page (just a table) + small mention on each structure info page
+const LigandTableRow = (props: LigandRowProps) => {
+    const ctx = useAppSelector(state => state.molstar.ui_plugin)
+
+    return <TableRow
+        className="hover:bg-slate-100   hover:cursor-pointer"
+    // onClick={props.connect_to_molstar_ctx ? () => { ctx == undefined ? console.log("Plugin is still loading") : selectChain(ctx!, polymer.auth_asym_id) } : undefined}
+    // onMouseEnter={props.connect_to_molstar_ctx ? () => { ctx == undefined ? console.log("Plugin is still loading") : highlightChain(ctx, polymer.asym_ids[0]) } : undefined}
+    // onMouseLeave={props.connect_to_molstar_ctx ? () => { ctx == undefined ? console.log("Plugin is still loading") : removeHighlight(ctx!) } : undefined} 
+    >
+        <TableCell>{props.ligandinfo.chemicalId}</TableCell>
+        <TableCell>{props.ligandinfo.chemicalName}</TableCell>
+        <TableCell><Badge variant="outline">{props.associated_structs.length}</Badge></TableCell>
+        <TableCell className="whitespace-pre">{props.ligandinfo.drugbank_id}</TableCell>
+        {/* <TableCell><DeleteIcon className="h-5 w-5" /></TableCell> */}
+    </TableRow>
+}
 
 export default function Ligands() {
-   
-    const { data:ligands_data, isLoading, isError } = useRoutersRouterStructListLignadsQuery()
+
+    const { data: ligands_data, isLoading, isError } = useRoutersRouterStructListLignadsQuery()
+
+
     useEffect(() => {
         console.log(ligands_data);
-        
+    }, [ligands_data])
 
+    return <ScrollArea className="border m-4 max-h-[95vh] rounded-md overflow-auto">
+        <Table >
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Chemical Id</TableHead>
+                    <TableHead>Name</TableHead>
+                    <TableHead># Host Structures</TableHead>
+                    
+                    <TableHead>Drugbank Info</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {
+                    ligands_data?.map((ligand, i) => {
+                        return <LigandTableRow key={i} ligandinfo={ligand[0]} associated_structs={ligand[1]} />
+                    })
+                }
+                <TableRow>
 
-    },[ligands_data])
-
-    return <Table>
-        <TableBody>
-            <TableRow>
-                <TableCell>1</TableCell>
-            </TableRow>
-        </TableBody>
-    </Table>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </ScrollArea>
 }
